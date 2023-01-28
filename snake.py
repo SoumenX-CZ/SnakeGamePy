9062e4c484acd44c779ac458a9e0b6e3ba9d90f8
SoumenX-CZ <90548394+SoumenX-CZ@users.noreply.github.com>
2023-01-28 17:24:53 +0100

    Backbround changed

--- snake.py
+++ snake.py
@@ -10,7 +10,7 @@ window_x = 720
 window_y = 480
 
 # defining colors
-black = pygame.Color(0, 0, 0)
+black = pygame.Color(255, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
